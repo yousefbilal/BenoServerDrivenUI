ace92910e7eeb38737f7ceb6e9d3658f868cf461
Yousef Irshaid <yousefbilal17@gmail.com>
2024-06-10 22:07:09 +0400

    added search bar composable

--- app/src/main/java/com/example/benoserverdrivenui/MainActivity.kt
+++ app/src/main/java/com/example/benoserverdrivenui/MainActivity.kt
@@ -8,49 +8,57 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.Button
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Shapes
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
-import com.example.benoserverdrivenui.ui.theme.BenoServerDrivenUITheme
+import com.example.benoserverdrivenui.ui.theme.FontBlack
+import com.example.benoserverdrivenui.ui.theme.FontDarkGrey
+import com.example.benoserverdrivenui.ui.theme.FontLightGrey
+import com.example.benoserverdrivenui.ui.theme.Green
+import com.example.benoserverdrivenui.ui.theme.LightGrey
+import com.example.benoserverdrivenui.ui.theme.White
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            BenoServerDrivenUITheme {
+
+            var text by remember {
+                mutableStateOf("")
+            }
+            Column {
+                SearchBar(value = text, onValueChange = { text = it }, modifier = Modifier.padding(horizontal = 24.dp))
 
                 ProductCard(
-                    modifier = Modifier.size(200.dp, 300.dp),
+                    modifier = Modifier.size(167.dp, 239.dp),
                     imageUrl = "https://s26552.pcdn.co/wp-content/uploads/2024/03/coffee.jpeg",
                     productName = "Coffee",
                     description = "Classic",
@@ -58,7 +66,9 @@ class MainActivity : ComponentActivity() {
                 )
 
             }
+
         }
+
     }
 }
 
@@ -71,49 +81,73 @@ fun ProductCard(
     price: String
 ) {
     Column(
-        modifier = Modifier
-            .clip(RoundedCornerShape(10.dp))
-            .background(Color.LightGray)
-            .then(modifier),
 
-        ) {
+        modifier = modifier
+            .clip(RoundedCornerShape(10.dp))
+            .background(White)
+
+    ) {
         AsyncImage(
             model = imageUrl,
             contentDescription = "product image",
             contentScale = ContentScale.Crop,
+//            alignment = Alignment.TopStart,
 
             modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(1F)
-                .padding(15.dp)
+//                .fillMaxWidth()
+//                .aspectRatio(1F)
+                .size(width = 141.dp, height = 132.dp)
+                .padding(start = 13.dp, end = 13.dp, top = 12.dp, bottom = 9.dp)
                 .clip(RoundedCornerShape(10.dp))
-                .size(10.dp)
         )
-        Text(text = productName, fontSize = 20.sp, modifier = Modifier.padding(15.dp, 0.dp))
+        Text(
+            text = productName,
+            color = FontBlack,
+            fontSize = 16.sp,
+            fontWeight = FontWeight(600),
+            modifier = Modifier
+                .padding(start = 13.dp, bottom = 4.dp)
+                .fillMaxWidth()
+        )
         Text(
             text = description,
             fontSize = 12.sp,
-            fontWeight = FontWeight.Light,
-            modifier = Modifier.padding(15.dp, 0.dp)
+            fontWeight = FontWeight(400),
+            color = FontLightGrey,
+            modifier = Modifier
+                .padding(start = 13.dp)
+                .fillMaxWidth()
         )
+
         Row(
-            Modifier.fillMaxWidth()
-                .padding(start = 15.dp, end = 15.dp),
+            Modifier
+                .fillMaxWidth()
+                .padding(start = 13.dp, end = 13.45.dp),
             horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-            
+            verticalAlignment = Alignment.Bottom
         ) {
 
             Text(
-                text = price, fontSize = 24.sp,
+                text = price,
+                color = FontDarkGrey,
+                fontSize = 18.sp,
+                fontWeight = FontWeight(600),
+                modifier = Modifier.padding(bottom = 19.dp)
             )
 
             Button(
                 shape = RoundedCornerShape(10.dp),
-                onClick = { /*TODO*/ }) {
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .padding(bottom = 12.dp)
+                    .width(35.87.dp),
+                contentPadding = PaddingValues(0.dp)
+
+            ) {
                 Icon(
                     imageVector = Icons.Rounded.Add,
-                    contentDescription = "add"
+                    contentDescription = "add",
+                    modifier = Modifier.padding(horizontal = 9.93.dp, vertical = 8.dp)
                 )
             }
         }
@@ -121,20 +155,55 @@ fun ProductCard(
     }
 }
 
+@Composable
+fun SearchBar(modifier: Modifier = Modifier, value: String, onValueChange: (String) -> Unit) {
+    BasicTextField(
+        value = value,
+        onValueChange = onValueChange,
+        textStyle = TextStyle(
+            fontSize = 16.sp
+        ),
+        decorationBox = { innerTextField ->
+            Box(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(LightGrey),
+            ) {
+                Icon(
+                    imageVector = Icons.Rounded.Search,
+                    contentDescription = "Search",
+                    tint = Green,
+                    modifier = Modifier
+                        .padding(start = 10.dp, top = 12.dp, bottom = 12.dp)
+                        .size(24.dp)
+                )
+                Spacer(modifier = Modifier.width(6.dp))
+                Box (
+                    modifier = Modifier.padding(top = 12.dp, start = 40.dp)
+                ){
+                    if (value.isEmpty()) {
+                        Text(
+                            "Search Coffee",
+                            fontWeight = FontWeight(400),
+                            fontSize = 16.sp,
+                            color = FontLightGrey,
+                        )
+                    }
+                    innerTextField()
+
+                }
+            }
+        }
+    )
+
+
+}
 
 @Preview
 @Composable
 private fun Prev() {
-    BenoServerDrivenUITheme {
-
-        ProductCard(
-            modifier = Modifier.size(200.dp, 300.dp),
-            imageUrl = "https://s26552.pcdn.co/wp-content/uploads/2024/03/coffee.jpeg",
-            productName = "Coffee",
-            description = "Classic",
-            price = "10$"
-        )
+    SearchBar(value = "") {
 
     }
-
 }
--- app/src/main/java/com/example/benoserverdrivenui/ui/theme/Color.kt
+++ app/src/main/java/com/example/benoserverdrivenui/ui/theme/Color.kt
@@ -1,11 +1,14 @@
 package com.example.benoserverdrivenui.ui.theme
-
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Green = Color(0xFF1BAE76)
+val LightGrey = Color(0xFFEFEFEF)
+val Grey = Color(0xFFF2F2F2)
+val Red = Color(0xFFFF5D5D)
+val Yellow = Color(0xFFFBBE21)
+val White = Color(0xFFFFFFFF)
+val FontLightGrey = Color(0xFF9B9B9B)
+val FontMediumGrey = Color(0xFF989898)
+val FontBlack = Color(0xFF2F2D2C)
+val FontDarkGrey = Color(0xFF464646)
