53bcf5033ec3d7e109b91dc92c5cf1d4af6c28ad
Yousef Irshaid <yousefbilal17@gmail.com>
2024-06-10 14:27:51 +0400

    initial commit

---
+++ app/src/main/java/com/example/benoserverdrivenui/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.benoserverdrivenui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.benoserverdrivenui.ui.theme.BenoServerDrivenUITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BenoServerDrivenUITheme {
+
+
+            }
+        }
+    }
+}
+
